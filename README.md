620f0d5924939633a6f54b1f87809481a3134157
Megan Steenkamp <megan.steenkamp@centrapay.com>
2024-07-09 16:05:00 +1000

    🎉 Centrapay checkout sample
    Create Centrapay checkout sample to easily enable integrators to understand and demo how to use Centrapay e-commerce.
    
    Test plan:
    - Have configured sample as per README.md and completed a payment.
    - Have asserted error messages show as expected.

--- README.md
+++ README.md
@@ -1,2 +1,36 @@
 # centrapay-checkout-sample
+
 Sample application using Centrapay SDK for the frontend and NodeJS for the backend.
+
+[Centrapay E-commerce Guide](https://docs.centrapay.com/guides/ecommerce-website/)
+
+## Run this project
+
+### Locally
+
+In your terminal run:
+
+`yarn install`
+
+`yarn start` and navigate to [http://localhost:6789/](http://localhost:6789/).
+
+### Frontend
+
+- Open `checkout.html` and replace the `test` string in the script tag with your Centrapay Merchant ID.
+
+### Backend
+
+- Rename the `.env.example` file to `.env`.
+- Inside of the `.env` file:
+  - Replace `YOUR_API_KEY` with your Centrapay API Key.
+  - Replace `YOUR_MERCHANT_CONFIG_ID` your Centrapay Merchant Config ID.
+- Run `yarn install` in your terminal
+- Run `yarn start` in your terminal
+
+## Legal
+
+Copyright © 2024 [Centrapay][].
+
+This software is licensed under Apache-2.0 License. Please see [LICENSE](/LICENSE) for details.
+
+[Centrapay]: https://centrapay.com/
---
+++ server/server.js
@@ -0,0 +1,96 @@
+import express from 'express';
+import fetch from 'node-fetch';
+import 'dotenv/config';
+import path from 'path';
+
+const {
+  CENTRAPAY_MERCHANT_API_KEY,
+  CENTRAPAY_MERCHANT_CONFIG_ID,
+  PORT = 6789,
+} = process.env;
+const base = 'https://service.cp42.click';
+const app = express();
+
+// Host static files
+app.use(express.static('client'));
+
+// Parse post params sent in body in json format
+app.use(express.json());
+
+// Create a Payment Request
+// https://docs.centrapay.com/api/payment-requests/#create-a-payment-request
+const createPaymentRequest = async (props) => {
+  const url = `${base}/api/payment-requests`;
+  const response = await fetch(url, {
+    headers: {
+      'Content-Type': 'application/json',
+      'X-Api-Key': CENTRAPAY_MERCHANT_API_KEY,
+    },
+    method: 'POST',
+    body: JSON.stringify({
+      configId: CENTRAPAY_MERCHANT_CONFIG_ID,
+      ...props,
+    }),
+  });
+
+  return handleResponse(response);
+};
+
+// Get a Payment Request
+// https://docs.centrapay.com/api/payment-requests/#get-a-payment-request
+const getPaymentRequest = async (props) => {
+  const { paymentRequestId } = props;
+  const url = `${base}/api/payment-requests/${paymentRequestId}`;
+  const response = await fetch(url, {
+    headers: {
+      'X-Api-Key': CENTRAPAY_MERCHANT_API_KEY,
+    },
+  });
+  return handleResponse(response);
+};
+
+async function handleResponse(response) {
+  try {
+    const jsonResponse = await response.json();
+    return {
+      jsonResponse,
+      httpStatusCode: response.status,
+    };
+  } catch (err) {
+    const errorMessage = await response.text();
+    throw new Error(errorMessage);
+  }
+}
+
+app.post('/api/payment-requests', async (req, res) => {
+  try {
+    const { jsonResponse, httpStatusCode } = await createPaymentRequest(
+      req.body,
+    );
+    res.status(httpStatusCode).json(jsonResponse);
+  } catch (error) {
+    console.error('Failed to create payment request:', error);
+    res.status(500).json({ error: 'Failed to create payment request.' });
+  }
+});
+
+app.get('/api/payment-requests/:paymentRequestId', async (req, res) => {
+  try {
+    const { jsonResponse, httpStatusCode } = await getPaymentRequest(
+      req.params,
+    );
+    res.status(httpStatusCode).json(jsonResponse);
+  } catch (error) {
+    console.error('Failed to get payment request:', error);
+    res.status(500).json({ error: 'Failed to get payment request.' });
+  }
+});
+
+// Serve index.html
+app.get('/', (req, res) => {
+  res.sendFile(path.resolve('./client/checkout.html'));
+});
+
+app.listen(PORT, () => {
+  console.log(`Node server listening at http://localhost:${PORT}/`);
+});
